551a0453c3ed2a1ab62c8cc1a6d35ff27668dc40
IMRANDIL <aliimranadil2@gmail.com>
2023-02-18 18:33:47 +0530

    soem more go

--- main.go
+++ main.go
@@ -8,6 +8,18 @@ import (
 
 
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found",http.StatusNotFound)
+		return
+	}
+
+
+}
+
+
+
+
 func main () {
 	fileServer := http.FileServer(http.Dir("./static"))
 
